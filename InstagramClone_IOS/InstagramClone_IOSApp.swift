614eb4a9c48c5b8b3c27e7f5a8cb5c6489877eba
Olivier Marszałkowski <marszalkowski.olivier.20@gmail.com>
2022-03-21 14:03:50 +0100

    Initial Commit

---
+++ InstagramClone_IOS/InstagramClone_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramClone_IOSApp.swift
+//  InstagramClone_IOS
+//
+//  Created by Olivier Marszałkowski on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramClone_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
